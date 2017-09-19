d5148730d2a001e33a419cf77d10e0a41981beb0
DESKTOP-CFD927E\clare <136592274@qq.com>
2017-09-19 17:16:59 +0800

    my works

--- resume/js/funcitions.js
+++ resume/js/funcitions.js
@@ -5,7 +5,9 @@ $(function(){
 	var btn = $("nav a");
 	var sections = $(".section");
 	var len = btn.length - 1;
-	win.scroll(function(){		
+	win.scroll(function(){	
+		
+		//导航定位
 		if(doc.scrollTop()>=840){
 			nav.addClass("fixed-nav");
 //			if(nav.css("opacity")!=1){
@@ -19,6 +21,7 @@ $(function(){
 		else 
 			nav.removeClass("fixed-nav");
 			
+		//滚动到相应位置导航栏自动切换	
 		btn.each(function(index){
 			var that = sections.eq(index);
 			if(doc.scrollTop() >= that.offset().top - 100){
@@ -27,13 +30,15 @@ $(function(){
 			}
 		});	
 		
-//		var skills = $(".skills");
-//		var education = $(".education");
-//		var bar = $(".progress-bar");
-//		if((doc.scrollTop()>=skills[0].top)&&(doc.scrollTop()<education[0].top)){
-//			bar.animate({color:"#000000"});
-//		}
-	});					
+		//progress-bar动画
+		var skills = $(".skills");
+		var education = $(".education");
+		var bar = $(".progress-bar");
+		if((doc.scrollTop()>=skills.offset().top - 400)&&(doc.scrollTop()<education.offset().top)){
+			bar.addClass("barGo");
+		}		
+	});			
+		//点击导航跳转
 			btn.on('click', function(e){
   			  	e.preventDefault();
 			  	$('html, body').animate({
